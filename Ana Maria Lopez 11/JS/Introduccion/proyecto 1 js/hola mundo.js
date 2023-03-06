d90a12e09ba3b39a0c750d0f884472a7bb00f752
ananode <125298938+ananode@users.noreply.github.com>
2023-03-06 13:19:23 -0500

    Agrego todos los trabajos

---
+++ Ana Maria Lopez 11/JS/Introduccion/proyecto 1 js/hola mundo.js
@@ -0,0 +1,12 @@
+console.log("Hola mundo!!");
+var a = 5;
+var b = 20;
+var suma = a + b;
+console.log(suma);
+// contar hasta 10
+for (let i = 0; i <=10; i++){
+    console.log(i)
+}
+for(let j = 100; j >=0; j--){
+    console.log(j)
+}
